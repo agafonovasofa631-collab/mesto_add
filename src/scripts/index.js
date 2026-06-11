f6ce3b32de4b941908621d9fc05a5b9052fb34f1
Агафонова Софья <agafonovasofa631@gmail.com>
2026-06-11 20:42:12 +0300

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -208,14 +208,14 @@ Promise.all([getCurrentUser(), getCards()])
 
     cards.forEach((card) => {
       placesWrap.append(
-        createCardElement(card, {
-          onPreviewPicture: handlePreviewPicture,
-          onLike: likeCardApi,
-          onUnlike: unlikeCardApi,
-          onDelete: handleDeleteCard,
-          onInfoClick: handleInfoClick,
-          currentUserId
-        })
+createCardElement(card, {
+  onPreviewPicture: handlePreviewPicture,
+  onLike: likeCardApi,
+  onUnlike: unlikeCardApi,
+  deleteCardApi: deleteCardApi,   // добавлено
+  onInfoClick: handleInfoClick,
+  currentUserId,
+});
       );
     });
   })
